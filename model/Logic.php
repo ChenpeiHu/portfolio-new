f475809d781371de9367f28af8dd30ff5d18d7df
Chen pei Hu <chenpeihu6@gmail.com>
2019-12-16 18:13:28 +0100

    undo

--- model/Logic.php
+++ model/Logic.php
@@ -3,12 +3,9 @@ require_once 'model/DataHandler.php';
 
 class Logic {
 	public function __construct() {
-        if($_SERVER['SERVER_ADDR'] == "51.91.158.178") {
-//            $this->DataHandler = new DataHandler("localhost", "mysql", "afspraakplanner", "root", "");
-            echo "";
-        }else{
-            $this->DataHandler = new DataHandler("localhost", "mysql", "afspraakplanner", "root", "");
-        }
+
+            //$this->DataHandler = new DataHandler("localhost", "mysql", "afspraakplanner", "root", "");
+
 	}
 
 	public function __destruct() {
