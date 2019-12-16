ba57535e3524bebc5b936c5469c3877526ef04ea
Chen pei Hu <chenpeihu6@gmail.com>
2019-12-16 18:10:22 +0100

    ja

--- model/Logic.php
+++ model/Logic.php
@@ -3,9 +3,12 @@ require_once 'model/DataHandler.php';
 
 class Logic {
 	public function __construct() {
-
-		$this->DataHandler   = new DataHandler( "localhost", "mysql", "afspraakplanner", "root", "" );
-
+        if($_SERVER['SERVER_ADDR'] == "51.91.158.178") {
+//            $this->DataHandler = new DataHandler("localhost", "mysql", "afspraakplanner", "root", "");
+            echo "";
+        }else{
+            $this->DataHandler = new DataHandler("localhost", "mysql", "afspraakplanner", "root", "");
+        }
 	}
 
 	public function __destruct() {
